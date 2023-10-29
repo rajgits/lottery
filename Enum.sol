efd49a994e8cc28ed7b883676a21e814a7f83468
rajgits14 <rajgits14@gmail.com>
2023-10-29 11:51:47 +0530

    enum on lottery

---
+++ Enum.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 < 0.9.0;
+
+contract State
+{
+    enum user{allowed,not_allowed,wait}
+
+    user public u1 = user.allowed;
+    uint public lottery = 1000;
+    function owner() public{
+        if(u1==user.allowed){
+            lottery = 0;
+        }
+    }
+
+    function ownerChange() public{
+        u1= user.wait;
+    }
+}
